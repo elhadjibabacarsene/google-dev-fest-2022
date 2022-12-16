a6380aba113aab2588039656e0a6370a50cf8a91
El Hadji Babacar Sene <babacar.sene@free-partenaires.sn>
2022-12-16 11:01:40 +0000

    data-update

--- lib/features/show_todo/data/datasources/todo_data_source.dart
+++ lib/features/show_todo/data/datasources/todo_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:google_dev_fest/features/show_todo/data/models/todo_model.dart';
+import 'package:http/http.dart' as http;
 
 import '../../domain/entities/todo.dart';
 
@@ -7,4 +8,22 @@ abstract class TodoDataSource {
   Future<TodoModel> createTodo(Todo todo);
   // Future<Either<Failure, String>> deleteTodo(String id);
   //Future<Either<Failure, String>> changeStatusTodo(String id);
+}
+
+class TodoDataSourceImpl implements TodoDataSource {
+  final http.Client client;
+  
+  const TodoDataSourceImpl({required this.client});
+
+  @override
+  Future<TodoModel> createTodo(Todo todo) {
+    // TODO: implement createTodo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<TodoModel>> getListTodo() {
+    // TODO: implement getListTodo
+    throw UnimplementedError();
+  }
 }
